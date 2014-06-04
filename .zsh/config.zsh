23bd3d8a60fc411dd2b42ce3e80a026efddb3387
Pontus Carlsson <pontus@thefarm.se>
2014-06-04 18:31:21 +0200

    Export ARCHCFLAG

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -81,3 +81,6 @@ export LC_ALL LANG
 # Which is a bit different on a Mac
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# XCode 5.1 Clang Compiler has issues with compiling Python C extensions
+export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
