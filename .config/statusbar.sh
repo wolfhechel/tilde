929b270bed9ebb93dc687f9851ab5f88632f966d
Pontus Jensen Karlsson <pontus@jensenkarlsson.se>
2020-12-17 20:36:24 +0100

    Added more information to the statusbar

--- .config/statusbar.sh
+++ .config/statusbar.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-read_battery() {
-    BAT0_dir=/sys/class/power_supply/BAT0
+battery_status() {
+    BAT_dir=/sys/class/power_supply/$1
     AC_dir=/sys/class/power_supply/AC
 
-    charge_now=$(cat ${BAT0_dir}/charge_now)
-    charge_full=$(cat ${BAT0_dir}/charge_full)
+    charge_now=$(cat ${BAT_dir}/charge_now)
+    charge_full=$(cat ${BAT_dir}/charge_full)
 
     level=`echo "scale=2;(${charge_now}/${charge_full})*100" | bc | cut -d'.' -f1`
 
@@ -24,17 +24,32 @@ read_battery() {
     echo "[${charging}] ${level}%"
 }
 
-get_nmcli_parameter() {
-    iface="$1"
-    param="$2"
 
-    nmcli -t -f $param device show $iface \
-        | cut -d':' -f2 \
-        | tr -d '[:space:]'
+wifi_ssid() {
+    nmcli -t -f GENERAL.CONNECTION device show $1 \
+        | cut -d':' -f2
 }
 
+wifi_signal_level() {
+    interface=$1
+    
+    cat /proc/net/wireless | awk -F' ' '$1 == "wlp2s0:" { print $4 }' \
+        | sed 's/\./dBm/g'
+}
 
-while true; do
-    echo "$(get_nmcli_parameter wlp2s0 GENERAL.CONNECTION) | $(read_battery) | $(date +%H:%M)"
+nv_temp() {
+    nvidia-smi stats -d temp -c 1 | cut -d' ' -f8
+}
+
+coretemp() {
+    echo $(cat /sys/class/hwmon/hwmon2/temp${1}_input) / 1000 | bc
+}
+
+line() {
+    echo "CPU: $(coretemp 1) | GPU: $(nv_temp) | $(wifi_ssid wlp2s0) ($(wifi_signal_level wlp2s0)) | $(pamixer --get-volume-human) | $(battery_status BAT0) | $(date +%H:%M)"
+}
+
+while true; do 
+    xsetroot -name "$(line)"
     sleep 1
 done
